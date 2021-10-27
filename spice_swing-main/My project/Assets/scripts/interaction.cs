28896158b05f57877edcb67d0e40ccd3e3bda2ae
PaytonNess <38738167+PaytonNess@users.noreply.github.com>
2021-10-27 11:42:31 -0500

    animations fixed

--- spice_swing-main/My project/Assets/scripts/interaction.cs
+++ spice_swing-main/My project/Assets/scripts/interaction.cs
@@ -4,29 +4,56 @@ using UnityEngine;
 
 public class interaction : MonoBehaviour
 {
-    private bool hasFood = false;
+    public bool hasMeat = false;
+    public bool hasGrain = false;
+    public bool hasVeggie = false;
+    public bool hasFood = false;
+    
     //needs to implement recipes
-    private bool hasCookFood = false;
+    public bool hasCookFood = false;
     private BoxCollider2D colid;
     private minigame mini;
     private Animator _anim;
-
+    private custumers orders;
+    public bool halfCooked = false;
     // Start is called before the first frame update
     void Start()
     {
         colid = GetComponent<BoxCollider2D>();
         mini = GetComponent<minigame>();
         _anim = GetComponent<Animator>();
+        orders = GetComponent<custumers>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+     /*   switch (orders.pos[orders.another].getOrder())
+        {
+            case 3:
+                
+                break;
+            case 2:
+                
+                break;
+            case 1:
+                
+                break;
+            case 0:
+                break;
+            default:
+                Debug.Log("error");
+                break;
+        } */
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        int regurallayer = _anim.GetLayerIndex("Base Layer"); ;
+        int rawlayer = _anim.GetLayerIndex("walking with raw"); ;
+        int layerfood = _anim.GetLayerIndex("walking with food"); ;
+        float wieght = 1f; 
+       
         if (other.transform.tag == "cooking")
         {
             if (hasFood)
@@ -38,13 +65,36 @@ public class interaction : MonoBehaviour
                 hasFood = false;
                 _anim.SetBool("raw", hasFood);
                 _anim.SetBool("food", hasCookFood);
-                mini.ActivateGame();
+                //mini.ActivateGame();
+                _anim.SetLayerWeight(rawlayer, 0);
+                _anim.SetLayerWeight(layerfood, wieght);
+
             }
         }
         if (other.transform.tag == "ingredents")
         {
             Debug.Log("has food");
             hasFood = true;
+            _anim.SetLayerWeight(regurallayer, -1);
+            _anim.SetLayerWeight(rawlayer, wieght);
+            _anim.SetBool("raw", hasFood);
+        }
+        if (other.transform.tag == "Meat")
+        {
+            Debug.Log("has food");
+            hasMeat = true;
+            _anim.SetBool("raw", hasFood);
+        }
+        if (other.transform.tag == "Veggie")
+        {
+            Debug.Log("has food");
+            hasVeggie = true;
+            _anim.SetBool("raw", hasFood);
+        }
+        if (other.transform.tag == "Grain")
+        {
+            Debug.Log("has food");
+            hasGrain = true;
             _anim.SetBool("raw", hasFood);
         }
         if (other.transform.tag == "serve")
@@ -58,6 +108,9 @@ public class interaction : MonoBehaviour
                 Debug.Log("served food");
                 custumers orders = other.gameObject.GetComponent<custumers>();
                 orders.leave();
+                _anim.SetLayerWeight(layerfood, 0);
+                _anim.SetLayerWeight(regurallayer, wieght);
+
             }
         }
     }
--- spice_swing-main/My project/Assets/scripts/movement.cs
+++ spice_swing-main/My project/Assets/scripts/movement.cs
@@ -17,38 +17,32 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float deltaX = 0;
         float deltaY = 0;
         if (Input.GetAxis("Horizontal") != 0)
-        {
+        {   
             deltaY = 0;
             deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            Vector2 movementX = new Vector2(deltaX, _body.velocity.y);
+            Vector2 movementX = new Vector2(deltaX, 0);
             _body.velocity = movementX;
+            //_anim.SetFloat("speedx", movementX);
         }
         else if (Input.GetAxis("Vertical") != 0)
-        {
+        {    
             deltaX = 0;
             deltaY = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            Vector2 movementY = new Vector2(_body.velocity.x, deltaY);
-            _body.velocity = movementY; 
+            Vector2 movementY = new Vector2(0, deltaY);
+            _body.velocity = movementY;
+            //_anim.SetFloat("speedy", movementY);
+        }
+        else
+        {
+            Vector2 movement = new Vector2(0, 0);
+            _body.velocity = movement;
         }
         _anim.SetFloat("speedx", deltaX);
-        //if (deltaX != 0)
-        //{
-        //   // Debug.Log("x");
-        //    Debug.Log(deltaX);
-        //}
-        ////if (!Mathf.Approximately(deltaX, 0))
-        ////{
-        ////    Debug.Log("help");
-        ////    transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
-        ////}
-        //if (deltaY != 0)
-        //{
-        //    //Debug.Log("y");
-        //    Debug.Log(deltaY);
-        //}    
+         
         _anim.SetFloat("speedy", deltaY);
         //this.transform.scale = 3;
         //transform.localScale = new Vector3(3, 3, 3);
